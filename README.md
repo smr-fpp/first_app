d0b8de1d6076a2149059e73938b3f650f35f2398
smr-fpp <105736014+smr-fpp@users.noreply.github.com>
2022-05-22 14:30:11 +0000

    [Finish] 2-2,2-3,2-4,2-5

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+2章【アプリケーションを作成しよう】
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2022_05_18_142137) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    
   end
 
 end
